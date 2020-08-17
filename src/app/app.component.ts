f5033886e536ca0e8a9af5e70f7f818d6e502eae
Fernando Zhunio <fzhunio91@hotmail.com>
2020-08-17 11:19:00 -0500

    Antes de instalar firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatfirebase';
+
+  username:string = 'Fernando Zhunio R.'
+  mensajes:{isMy:boolean,type:'string'|'File',content:any,date?:Date}[]=[
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:true,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+    {isMy:false,type:'string',content:'Mensaje de prueba'},
+  ]
 }
